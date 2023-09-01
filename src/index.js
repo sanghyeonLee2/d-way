3683dee3027a95fa18a333025ef1addd89b7fc3d
sanghyeonLee <95309008+sanghyeonLee2@users.noreply.github.com>
2023-09-01 15:48:21 +0900

    feat: 라우터 구조 정의

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import ReactDOM from "react-dom/client";
+import SignIn from "./routes/SignIn";
+import Home from "./routes/Home";
+import SignUp from "./routes/SignUp";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
